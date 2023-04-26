caba5c73e22b17840ae1accd9da4ea61e4becaa6
Clyde Catap <clydebryoncatap@gmail.com>
2023-04-26 21:08:34 +0800

    RPS w/ buttons

--- rps.js
+++ rps.js
@@ -3,42 +3,73 @@ function GetComputerchoice() {
     return choices[Math.floor(Math.random()*choices.length)]
 }
 
-function GetPlayerchoice(){
-    let player_answer = prompt("What's Your choice Rock, Paper, or Scissors??")
-    return player_answer
+
+let Score = 0;
+let comp_score = 0;
+
+const div = document.querySelector('#container');
+const p = document.createElement('p');
+const c = document.createElement('p');
+const w = document.createElement('h2');
 
 
+
+document.querySelectorAll('button').forEach(x =>{
+    let player = x.getAttribute('id');
+    x.addEventListener('click', function() {
+        let comp = GetComputerchoice();
+
+        if (Score == 5) {
+            w.textContent = 'YOU WIN!!!';
+            div.appendChild(w);
+        }
+
+        else if (comp_score == 5) {
+            w.textContent = 'YOU LOSE!!!';
+            div.appendChild(w);
+        }
+
+
+        else if (player == comp) {
+            p.textContent = 'Your Score: ' + Score;
+            c.textContent = 'Comp Score: ' + comp_score;
+            div.appendChild(p);
+            div.appendChild(c);
+
+            return console.log("Draw :p"), console.log( "Your Score: " + Score), console.log( "Computer Score: " + comp_score);
+        }
+        
+
+        else if ((player == "Rock" && comp == "Scissors") || (player == "Paper" && comp == "Rock") || (player == "Scissors" && comp == "Paper") ) {
+            Score += 1 
+            
+            p.textContent = 'Your Score: ' + Score;
+            c.textContent = 'Comp Score: ' + comp_score;
+            div.appendChild(p);
+            div.appendChild(c);
+
+
+            return console.log("You win"), console.log( "Your Score: " + Score), console.log( "Computer Score: " + comp_score);
+        }
+
+
+        else {
+            comp_score += 1 
+            
+            p.textContent = 'Your Score: ' + Score;
+            c.textContent = 'Comp Score: ' + comp_score;
+            div.appendChild(p);
+            div.appendChild(c);
+
+
+            return console.log("You Lose"), console.log( "Your Score: " + Score), console.log( "Computer Score: " + comp_score);
+        
+        }
+
+
+    } 
+    );
 }
+);
 
 
-function Playgame() {
-
-    let Score = 0;
-    let comp_score = 0;
-    let player = GetComputerchoice();
-    let comp = GetPlayerchoice();
-
-if (player == comp) {
-
-    return console.log("Draw :p"), console.log( "Your Score: " + Score), console.log( "Computer Score: " + comp_score);
-
-}
-
-else if ((player == "Rock" && comp == "Scissors") || (player == "Paper" && comp == "Rock") || (player == "Scissors" && comp == "Paper") ) {
-    Score += 1 
-    return console.log("You win"), console.log( "Your Score: " + Score), console.log( "Computer Score: " + comp_score);
-
-}
-else {
-    comp_score += 1 
-    return console.log("You Lose"), console.log( "Your Score: " + Score), console.log( "Computer Score: " + comp_score);
-
-}
-}
-
-let number_of_rounds = prompt("How many rounds")
-
-for (let i = 0; i < number_of_rounds; i++)
-{
-    let final_score = Playgame();
-}
